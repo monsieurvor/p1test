010edc7188b33c6cd7ad447e1b5f6eff64af52a8
Yvar Dankers <yvardankers@gmail.com>
2024-02-24 19:11:08 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,33 @@
 
 unsigned long lastReading = 0;
 
+// void setup() {
+//   Serial.begin(BAUD_RATE);
+//   while (!Serial)
+//   {
+//     vTaskDelay(2);
+//   }
+//   Serial.println("Starting...");
+//   Serial2.begin(BAUD_RATE, SERIAL_8N1, RXD2, TXD2, true);
+//   while (!Serial2)
+//   {
+//     vTaskDelay(2);
+//   }
+//   Serial.println("Serial 2 started");
+//   setupDataReadout();
+// }
+
+// void loop()
+// {
+//     unsigned long now = millis();
+//     if (now - lastReading > READ_INTERVAL)
+//     {
+//         readP1Serial();
+//         lastReading = millis();
+//     }
+
+// }
+
 void setup() {
   Serial.begin(BAUD_RATE);
   while (!Serial)
@@ -16,16 +43,24 @@ void setup() {
     vTaskDelay(2);
   }
   Serial.println("Serial 2 started");
-  setupDataReadout();
+  Serial.println("Starting...");
+  unsigned long startTime = millis();
+  while (Serial2.available() || millis() - startTime < 5000)
+  {
+      char c = Serial2.read();
+      Serial.print(c);
+  }
+  Serial.println("");
+  Serial.println("Done");
 }
 
 void loop()
 {
-    unsigned long now = millis();
-    if (now - lastReading > READ_INTERVAL)
-    {
-        readP1Serial();
-        lastReading = millis();
-    }
+    // unsigned long now = millis();
+    // if (now - lastReading > READ_INTERVAL)
+    // {
+    //     readP1Serial();
+    //     lastReading = millis();
+    // }
 
 }
